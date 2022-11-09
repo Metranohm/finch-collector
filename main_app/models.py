c58d09da48cd9a24e2d7185c8f1e02826d9b739a
Metranohm <andrewwink@gmail.com>
2022-11-09 12:35:24 -0600

    implement create, delete, update CBVs

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   species = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
-
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  
   def __str__(self):
-    return self.name
+    return f"{self.get_meal_display()} on {self.date}"
+
+  def get_absolute_url(self):
+    return reverse('finches_detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 
 # Create your views here.
@@ -16,3 +18,16 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finches/'
+  
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['species', 'description', 'age']
+  
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
